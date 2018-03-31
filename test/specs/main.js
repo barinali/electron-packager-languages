637724e4e9ecc8c611a9a729412eb4398aa53a73
Felipe Castillo <fcastillo.ec@gmail.com>
2018-03-31 16:36:49 -0400

    Add test for Linux

--- test/specs/main.js
+++ test/specs/main.js
@@ -5,7 +5,7 @@ const packager = require('electron-packager');
 const setLanguages = require('../../src/index');
 
 const APP_NAME = 'app';
-const LANGUAGES = ['en', 'en_GB'];
+const LANGUAGES = ['en', 'en_GB', 'en-US'];
 
 const packagerOptions = {
   afterCopy: [setLanguages(LANGUAGES)],
@@ -25,23 +25,34 @@ const testCase = function (err, appPaths) {
     assert.fail('The packaging process is failed!');
   }
   const appPath = path.resolve('.', appPaths[0]);
-  const resourcePath = path.resolve(appPath, `${APP_NAME}.app/Contents/Resources`);
-  const resources = fs.readdirSync(resourcePath);
-  const languageFolderFilter = new RegExp(`\.(lproj)$`);
-  const languageFolders = resources.filter((folder) => languageFolderFilter.test(folder));
+  if (appPaths[0].includes('darwin') || appPaths[0].includes('mas')) {
+    const resourcePath = path.resolve(appPath, `${APP_NAME}.app/Contents/Resources`);
+    const resources = fs.readdirSync(resourcePath);
+    const languageFolderFilter = new RegExp(`\.(lproj)$`);
+    const languageFolders = resources.filter((folder) => languageFolderFilter.test(folder));
 
-  languageFolders.forEach((languageFolder) => {
-    const language = languageFolder.split('.')[0];
+    languageFolders.forEach((languageFolder) => {
+      const language = languageFolder.split('.')[0];
 
-    if (!LANGUAGES.includes(language)) {
-      assert.fail(`This language, "${language}", shouldn't be in the app.`);
-    }
-  });
+      if (!LANGUAGES.includes(language)) {
+        assert.fail(`This language, "${language}", shouldn't be in the app.`);
+      }
+    });
+  } else if (appPaths[0].includes('linux')) {
+    const languageFolder = path.resolve(appPath, 'locales');
+    const languagePacks = fs.readdirSync(languageFolder);
+    languagePacks.forEach((language) => {
+      if (!LANGUAGES.includes(language.split('.')[0])) {
+        assert.fail(`This language, "${language}", shouldn't be in the app.`);
+      }
+    })
+  }
 
   console.log(`Tests are passed for ${appPath}`);
 };
 
-(async function() {  
+(async function() {
   await testPlatform('darwin', testCase);
   await testPlatform('mas', testCase);
+  await testPlatform('linux', testCase);
 })();
