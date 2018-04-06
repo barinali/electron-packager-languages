d0a0b02625399d1232bcc91ed97ab150b08a8c3d
Ali BARIN <ali.barin53@gmail.com>
2018-04-06 20:13:42 +0200

    chore: Add eslint

--- src/index.js
+++ src/index.js
@@ -8,24 +8,27 @@ function getResourcePath(givenPath) {
 
 function walkDir(dir, regex = /./g) {
   return fs.readdirSync(dir)
-    .filter((currentPath) => fs.statSync(path.resolve(dir, currentPath)).isDirectory() && regex.test(currentPath));
+    .filter(currentPath =>
+      fs.statSync(path.resolve(dir, currentPath)).isDirectory() && regex.test(currentPath));
 }
 
 // eslint-disable-next-line max-params
-module.exports = function languages(languages) {
-  return function(buildPath, electronVersion, platform, arch, callback) {
+module.exports = function setLanguages(languages) {
+  return function electronPackagerLanguages(buildPath, electronVersion, platform, arch, callback) {
     // eslint-disable-next-line
     if (['darwin', 'mas'].includes(platform)) {
       const resourcePath = getResourcePath(buildPath);
-      const excludedLanguages = languages.map((l) => `${l}.lproj`);
-      const languageFolderFilter = new RegExp(`\.(lproj)$`);
+      const excludedLanguages = languages.map(l => `${l}.lproj`);
+      const languageFolderFilter = new RegExp('.(lproj)$');
 
       const languageFolders = walkDir(resourcePath, languageFolderFilter);
-      languageFolders.filter((langFolder) => !excludedLanguages.includes(langFolder)).forEach((langFolder) => {
-        rimraf.sync(path.resolve(resourcePath, langFolder))
-      });
+      languageFolders
+        .filter(langFolder => !excludedLanguages.includes(langFolder))
+        .forEach((langFolder) => {
+          rimraf.sync(path.resolve(resourcePath, langFolder));
+        });
     }
 
     callback();
-  }
-}
+  };
+};
