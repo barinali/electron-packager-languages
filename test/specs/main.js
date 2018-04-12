707b75c4309071394aad6aded6f0ba794cf586dd
Felipe Castillo <fcastillo.ec@gmail.com>
2018-04-12 04:07:23 -0400

    Fix tests to support all platforms

--- test/specs/main.js
+++ test/specs/main.js
@@ -5,7 +5,7 @@ const packager = require('electron-packager');
 const setLanguages = require('../../src/index');
 
 const APP_NAME = 'app';
-const LANGUAGES = ['en', 'en_GB'];
+const LANGUAGES = ['en', 'en_GB', 'en-US'];
 
 const packagerOptions = {
   afterCopy: [setLanguages(LANGUAGES)],
@@ -18,17 +18,52 @@ const packagerOptions = {
   prune: false,
 };
 
-const testPlatform = (platform, done) => packager({ ...packagerOptions, platform }, done);
-
-function testCase(err, appPaths) {
-  if (err) {
-    assert.fail('The packaging process is failed!');
+function getLanguageFolderPath(givenPath, platform) {
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return path.resolve(givenPath, `${APP_NAME}.app/Contents/Resources`);
+    case 'win32':
+    case 'linux':
+      return path.resolve(givenPath, 'locales');
+    default:
+      return path.resolve(givenPath);
   }
+}
+
+function getLanguageFileExtension(platform) {
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return 'lproj';
+    case 'win32':
+    case 'linux':
+      return 'pak';
+    default:
+      return '';
+  }
+}
+
+function walkLanguagePaths(dir, platform) {
+  const regex = new RegExp(`.(${getLanguageFileExtension(platform)})$`);
+  const paths = fs.readdirSync(dir);
+
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return paths.filter(folder => regex.test(folder));
+    case 'win32':
+    case 'linux':
+      return paths;
+    default:
+      return [];
+  }
+}
+
+function testCase(appPaths, platform) {
   const appPath = path.resolve('.', appPaths[0]);
-  const resourcePath = path.resolve(appPath, `${APP_NAME}.app/Contents/Resources`);
-  const resources = fs.readdirSync(resourcePath);
-  const languageFolderFilter = new RegExp('.(lproj)$');
-  const languageFolders = resources.filter(folder => languageFolderFilter.test(folder));
+  const resourcePath = getLanguageFolderPath(appPath, platform);
+  const languageFolders = walkLanguagePaths(resourcePath, platform);
 
   languageFolders.forEach((languageFolder) => {
     const language = languageFolder.split('.')[0];
@@ -42,7 +77,16 @@ function testCase(err, appPaths) {
   console.log(`Tests are passed for ${appPath}`);
 }
 
+function testPlatform(platform) {
+  return packager({ ...packagerOptions, platform })
+    .then(appPaths => testCase(appPaths, platform))
+    .catch(err => assert.fail(err, 'The packaging process is failed!'));
+}
+
+
 (async function testRun() {
-  await testPlatform('darwin', testCase);
-  await testPlatform('mas', testCase);
+  await testPlatform('darwin');
+  await testPlatform('mas');
+  await testPlatform('linux');
+  await testPlatform('win32');
 }());
