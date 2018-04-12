9798d0e26d380d298ff0cda9e85ca3d4163f142a
Felipe Castillo <fcastillo.ec@gmail.com>
2018-04-12 04:06:15 -0400

    Add support for linux and windows

--- src/index.js
+++ src/index.js
@@ -2,30 +2,58 @@ const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 
-function getResourcePath(givenPath) {
-  return path.resolve(givenPath, '..');
+function getLanguageFolderPath(givenPath, platform) {
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return path.resolve(givenPath, '..');
+    case 'win32':
+    case 'linux':
+      return path.resolve(givenPath, '..', '..', 'locales');
+    default:
+      return path.resolve(givenPath);
+  }
 }
 
-function walkDir(dir, regex = /./g) {
-  return fs.readdirSync(dir)
-    .filter(currentPath =>
-      fs.statSync(path.resolve(dir, currentPath)).isDirectory() && regex.test(currentPath));
+function getLanguageFileExtension(platform) {
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return 'lproj';
+    case 'win32':
+    case 'linux':
+      return 'pak';
+    default:
+      return '';
+  }
+}
+
+function walkLanguagePaths(dir, platform) {
+  const regex = new RegExp(`.(${getLanguageFileExtension(platform)})$`);
+  const paths = fs.readdirSync(dir);
+
+  switch (platform) {
+    case 'darwin':
+    case 'mas':
+      return paths.filter(currentPath => fs
+        .statSync(path.resolve(dir, currentPath)).isDirectory() && regex.test(currentPath));
+    case 'win32':
+    case 'linux':
+      return paths;
+    default:
+      return [];
+  }
 }
 
 module.exports = function setLanguages(languages) {
   return function electronPackagerLanguages(buildPath, electronVersion, platform, arch, callback) {
-    if (['darwin', 'mas'].includes(platform)) {
-      const resourcePath = getResourcePath(buildPath);
-      const excludedLanguages = languages.map(l => `${l}.lproj`);
-      const languageFolderFilter = new RegExp('.(lproj)$');
+    const resourcePath = getLanguageFolderPath(buildPath, platform);
+    const excludedLanguages = languages.map(l => `${l}.${getLanguageFileExtension(platform)}`);
+    const languageFolders = walkLanguagePaths(resourcePath, platform);
 
-      const languageFolders = walkDir(resourcePath, languageFolderFilter);
-      languageFolders
-        .filter(langFolder => !excludedLanguages.includes(langFolder))
-        .forEach((langFolder) => {
-          rimraf.sync(path.resolve(resourcePath, langFolder));
-        });
-    }
+    languageFolders
+      .filter(langFolder => !excludedLanguages.includes(langFolder))
+      .forEach(langFolder => rimraf.sync(path.resolve(resourcePath, langFolder)));
 
     callback();
   };
